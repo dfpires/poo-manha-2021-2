f4bca9bee093103a13e3fb26ac464682835df159
Daniel Pires <dfpires@gmail.com>
2021-09-08 09:03:49 -0300

    08-09

--- lista4/Diretor.java
+++ lista4/Diretor.java
@@ -3,4 +3,23 @@ package lista4;
 public class Diretor extends Funcionario{
     // o extends faz com que diretor tenha nome, cpf e salário do pai
     // o extends faz com que diretor tenha getters, setters e toString do pai
+    private String carro;
+    public Diretor(){
+        super();
+    }
+    public Diretor(String nome, String cpf, float salario, String carro) {
+        super(nome, cpf, salario);
+        this.setCarro(carro);
+    }
+    public String getCarro() {
+        return carro;
+    }
+    public void setCarro(String carro) {
+        this.carro = carro;
+    }
+   @Override
+   public String toString(){
+       return super.toString() + " Carro: " + this.carro;
+   }
+    
 }
