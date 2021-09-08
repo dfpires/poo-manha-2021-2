7290cd57a708180950ed70e42b652c54b8d4d057
Daniel Pires <dfpires@gmail.com>
2021-09-08 09:00:17 -0300

    08-09

--- lista4/Gerente.java
+++ lista4/Gerente.java
@@ -3,4 +3,25 @@ package lista4;
 public class Gerente extends Funcionario{
     // o extends faz com que gerente tenha nome, cpf e salário do pai
     // o extends faz com que gerente tenha getters, setters e toString do pai
+    private float bonus;
+    public Gerente() {
+        super(); // chama construtor da superclasse para reutilização de código
+    }
+  
+    public Gerente(String nome, String cpf, float salario, float bonus) {
+        super(nome, cpf, salario); // chama construtor da superclasse para reutilização de código
+        this.setBonus(bonus);
+    }
+
+    public float getBonus() {
+        return bonus;
+    }
+    public void setBonus(float bonus) {
+        this.bonus = bonus;
+    }
+    @Override
+    public String toString(){
+        return super.toString() + " bônus " + this.bonus;
+    }
+
 }
