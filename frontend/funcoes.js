deb84e22bc73fd46c88791555cbc27b417566adb
Daniel Pires <dfpires@gmail.com>
2021-10-25 09:05:19 -0300

    25-10

--- frontend/funcoes.js
+++ frontend/funcoes.js
@@ -22,6 +22,29 @@ function insere(){
     req.setRequestHeader('Content-Type', 'application/json')
     // converte objeto JSON em string
     req.send(JSON.stringify(objeto))
-    console.log('Requisição enviada com sucesso')
+    alert('Paciente inserido com sucesso')
+    carregaTabela()
+}
 
+function carregaTabela(){
+     // cria objeto de requisição
+     let req = new XMLHttpRequest()
+     // abrir a requisição
+     req.open('GET', 'http://localhost:8080/paciente', true) // assíncrona
+     req.onload = function () {
+
+         // recupera os dados do banco e converte para JSON
+        let pacientes = JSON.parse(this.response)
+        let conteudo = ""
+        pacientes.forEach(element => {
+            conteudo = conteudo + `<tr> <td> ${element.id} </td> <td> ${element.cpf} </td> <td> ${element.peso} </td> <td> ${element.altura} </td> <td> ${element.idade} </td> <td> <i class="bi bi-trash"></i> </td> <td> <i class="bi bi-pencil"></i> </td> </tr>`
+        });
+        // envia os dados para o HTML
+        document.getElementById("conteudoTabela").innerHTML = conteudo
+        // configura o cabeçalho de requisição
+
+     }
+   
+     req.setRequestHeader('Content-Type', 'application/json')
+     req.send()
 }
