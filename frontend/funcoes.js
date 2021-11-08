db634e2787241d40716725f9112c5d86e30b8514
Daniel Pires <dfpires@gmail.com>
2021-11-08 08:05:02 -0300

    08-11

--- frontend/funcoes.js
+++ frontend/funcoes.js
@@ -6,6 +6,8 @@ function insere(){
     let altura = Number(document.getElementById("altura").value)
     let idade = Number(document.getElementById("idade").value)
 
+    let id = Number(document.getElementById("id").value)
+  
     // criando um objeto
     let objeto = {
         "nome": nome,
@@ -14,15 +16,25 @@ function insere(){
         "altura": altura,
         "idade": idade
     }
+
+    let verbo // conterá POST ou PUT
+    if (!id){ // estamos inserindo
+        verbo = 'POST' // insere
+    }
+    else {
+        verbo = 'PUT' // atualiza
+        objeto.id = id // coloca id no objeto
+    }
+
     // cria objeto de requisição
     let req = new XMLHttpRequest()
     // abrir a requisição
-    req.open('POST', 'http://localhost:8080/paciente', true) // assíncrona
+    req.open(verbo, 'http://localhost:8080/paciente', true) // assíncrona
     // configura o cabeçalho de requisição
     req.setRequestHeader('Content-Type', 'application/json')
     // converte objeto JSON em string
     req.send(JSON.stringify(objeto))
-    alert('Paciente inserido com sucesso')
+    alert('Paciente inserido/atualizado com sucesso')
     carregaTabela()
 }
 
@@ -37,8 +49,13 @@ function remove(id){
         carregaTabela()// atualiza a lista de pacientes
     }
 }
-function atualiza(){
-    alert(`Vamos atualizar`)
+function atualiza(id, nome, cpf, peso, altura, idade){
+    document.getElementById("nome").value = nome
+    document.getElementById("cpf").value = cpf
+    document.getElementById("peso").value = peso
+    document.getElementById("altura").value = altura
+    document.getElementById("idade").value = idade
+    document.getElementById("id").value = id
 }
 function carregaTabela(){
      // cria objeto de requisição
@@ -51,7 +68,7 @@ function carregaTabela(){
         let pacientes = JSON.parse(this.response)
         let conteudo = ""
         pacientes.forEach(element => {
-            conteudo = conteudo + `<tr> <td> ${element.id} </td> <td> ${element.cpf} </td> <td> ${element.peso} </td> <td> ${element.altura} </td> <td> ${element.idade} </td> <td> <button onclick="remove(${element.id})">  <i class="bi bi-trash"></i> </button> </td> <td> <button onclick="atualiza()"> <i class="bi bi-pencil"></i> </button></td> </tr>`
+            conteudo = conteudo + `<tr> <td> ${element.id} </td> <td> ${element.nome} </td> <td> ${element.cpf} </td> <td> ${element.peso} </td> <td> ${element.altura} </td> <td> ${element.idade} </td> <td> <button onclick="remove(${element.id})">  <i class="bi bi-trash"></i> </button> </td> <td> <button onclick="atualiza(${element.id}, ${element.nome}, ${element.cpf}, ${element.peso}, ${element.altura}, ${element.idade})"> <i class="bi bi-pencil"></i> </button></td> </tr>`
         });
         // envia os dados para o HTML
         document.getElementById("conteudoTabela").innerHTML = conteudo
